7f04903957cd6bf7114e3793bfdf028b6b18fee2
RMLaroche <RMLaroche@users.noreply.github.com>
2019-02-28 11:46:32 +0100

    Initial commit

---
+++ src/Integ/Calculs.java
@@ -0,0 +1,69 @@
+package Integ;
+
+public class Calculs
+
+{
+
+	private final int operande1;
+
+	private final int operande2;
+
+	public Calculs(int operande1, int operande2)
+
+	{
+
+		this.operande1 = operande1;
+
+		this.operande2 = operande2;
+
+	}
+
+	public int multiplier()
+
+	{
+
+		return operande1 * operande2;
+
+	}
+
+	public int additionner()
+
+	{
+
+		return operande1 + operande2;
+
+	}
+
+	public int diviser()
+
+	{
+	//	if(this.operande2 == 0) {
+	//	System.out.println("DIVISION PAR 0 !");
+	//	return 0;
+	//	}
+	//	else {
+		return operande1 / operande2;	
+	//	}
+		
+
+	}
+
+	public int soustraire()
+
+	{
+
+		return operande1 - operande2;
+
+	}
+	
+	public static void main(String[] args) {
+		Calculs calculs = new Calculs(-8454, 0);
+		
+		System.out.println(calculs.multiplier());
+		System.out.println(calculs.additionner());
+		System.out.println(calculs.diviser());
+		System.out.println(calculs.soustraire());
+		
+	}
+
+}
---
+++ src/Integ/CalculsTest.java
@@ -0,0 +1,52 @@
+package Integ;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculsTest {
+	
+	protected int operande1 = 6;
+	protected int operande2 = 9;
+	Calculs calculs ;
+
+	@BeforeEach
+	void setUp() throws Exception {			//ce qui va	être éxécuté avant
+		
+		this.calculs = new Calculs(operande1,operande2);   //instancier objet calcul
+		//attribuer des valeurs
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+
+	@Test
+	void testMultiplier() {
+		assertEquals(54, this.calculs.multiplier());
+		//assertequals
+	}
+
+	@Test
+	void testAdditionner() {
+		assertEquals(15, this.calculs.additionner());
+	}
+
+	@Test
+	void testDiviser() {
+		assertEquals((6/9), this.calculs.diviser());
+		Calculs calculs2 = new Calculs(8,0);
+		Assertions.assertThrows(ArithmeticException.class,()->{calculs2.diviser();});
+			
+	}
+
+	@Test
+	void testSoustraire() {
+		assertEquals(-3, this.calculs.soustraire());
+	}
+
+}
